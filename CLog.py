3f558ab6f6e4ee5f154740ddddb00c64c1c21048
frank.yao <frank.yao@vipshop.com>
2014-03-28 18:30:04 +0800

    rename class name to CLog and add file_name in chain

--- CLog.py
+++ CLog.py
@@ -1,7 +1,9 @@
-__author__ = 'frankyao'
+__author__ = 'baniu.yao'
+
+
 import inspect
 import logging
-import os
+
 
 def get_class_from_frame(fr):
     args, _, _, value_dict = inspect.getargvalues(fr)
@@ -12,24 +14,24 @@ def get_class_from_frame(fr):
     return None
 
 
-class Log(object):
+class CLog(object):
     def __init__(self, log_file_path='./test.log'):
         logging.basicConfig(filename=log_file_path,
                             level=logging.DEBUG, filemode='aw',
                             format='%(asctime)s [%(chain)s] %(message)s')
+
     def get_meta_data(self):
         frame = inspect.stack()[2][0]
         current_frame = inspect.currentframe()
         caller_frame = inspect.getouterframes(current_frame, 2)
         args, _, _, value_dict = inspect.getargvalues(frame)
         caller_class_name = get_class_from_frame(frame).__name__
-        caller_list = []
-        for frame in caller_frame[-2:0:-1]:
+        file_name = caller_frame[-1][1].split('/')[-1]
+        caller_list = [file_name, caller_class_name]
+        for frame in caller_frame[-2:1:-1]:
             caller_list.append(frame[3])
-        return '-->'.join([caller_class_name] + caller_list)
+        return ' --> '.join(caller_list)
 
     def write(self, message):
         chain = self.get_meta_data()
-        logging.info(message,
-                     extra={'chain': chain}
-        )
+        logging.info(message, extra={'chain': chain})
